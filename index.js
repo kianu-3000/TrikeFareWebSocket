316c9cf11e0662bb50b36ed988815e0dba3021b1
Kianu <kianuyepes3000@gmail.com>
2025-10-05 21:33:08 +0800

    added driver cancel

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ app.post('/notify', (req, res) => {
         io.emit('booking_canceled', data); // user will cancel
     }
 
+    if (event === 'driver_cancelled') {
+        console.log(data)
+        io.to(`user_${data.user_id}`).emit('booking_canceled', data); // driver will cancel
+    }
+
     res.send({ status: 'ok' });
 });
 const PORT = process.env.PORT || 4000;
